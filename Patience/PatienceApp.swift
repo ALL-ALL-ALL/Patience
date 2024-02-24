8794ed6d478e8d085b7558c772c4baf42b9b3e4a
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-24 02:01:43 +0100

    Initial Commit

---
+++ Patience/PatienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatienceApp.swift
+//  Patience
+//
+//  Created by  Ixart on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PatienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
